78a82f23f7980593d9af6f58c590249c303c468c
Wesdell <wesdell17@gmail.com>
2024-12-01 18:09:05 -0500

    Mark up welcome auth page and link auth routes

---
+++ app/(root)/(auth)/welcome.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Welcome() {
+  return (
+    <SafeAreaView>
+      <Text>Welcome</Text>
+    </SafeAreaView>
+  );
+}
