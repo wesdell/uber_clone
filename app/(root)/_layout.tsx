52736d976d772ac6f1b2fd113b76d197ba81eb07
Wesdell <wesdell17@gmail.com>
2024-12-01 21:24:54 -0500

    Remove routes header

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack></Stack>;
+  return (
+    <Stack>
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
