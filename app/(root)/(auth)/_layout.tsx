818f7b319c0deb01eb76580a2cd5e0bbc8c18648
Wesdell <wesdell17@gmail.com>
2024-12-03 20:22:50 -0500

    Redirect user to home page if has a session

--- app/(root)/(auth)/_layout.tsx
+++ app/(root)/(auth)/_layout.tsx
@@ -1,6 +1,13 @@
-import { Stack } from "expo-router";
+import { Redirect, Stack } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
 
 export default function AuthLayout() {
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) {
+    return <Redirect href="/(root)/(tabs)/home" />;
+  }
+
   return (
     <Stack>
       <Stack.Screen name="welcome" options={{ headerShown: false }} />
