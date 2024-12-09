339feaa58b22cf7ffb375ca6f5a3b806eafe2427
Wesdell <wesdell17@gmail.com>
2024-12-08 22:45:09 -0500

    Use driver and user location state to display its location icon in the map

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,16 +1,67 @@
-import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
+import { useEffect, useState } from "react";
+import MapView, { Marker, PROVIDER_DEFAULT } from "react-native-maps";
+import { useDriverStore, useLocationStore } from "@/store";
+import { calculateRegion, generateMarkersFromData } from "@/lib/map";
+import { driversMock } from "@/mocks/driver";
+import { MarkerData } from "@/types/type";
+import { icons } from "@/constants";
 
 const Map = ({ height }: { height: number }) => {
+  const {
+    userLongitude,
+    userLatitude,
+    destinationLongitude,
+    destinationLatitude,
+  } = useLocationStore();
+  const { selectedDriver, setDrivers } = useDriverStore();
+  const region = calculateRegion({
+    userLongitude,
+    userLatitude,
+    destinationLongitude,
+    destinationLatitude,
+  });
+  const [markers, setMarkers] = useState<MarkerData[]>([]);
+
+  useEffect(() => {
+    if (Array.isArray(driversMock)) {
+      if (!userLatitude || !userLongitude) {
+        return;
+      }
+    }
+
+    const newMarkers = generateMarkersFromData({
+      data: driversMock,
+      userLatitude,
+      userLongitude,
+    });
+    setMarkers(newMarkers);
+  }, [userLatitude, userLongitude]);
+
   return (
     <MapView
       provider={PROVIDER_DEFAULT}
+      initialRegion={region}
       style={{ height: height }}
       tintColor="black"
       mapType="mutedStandard"
       showsPointsOfInterest={false}
       showsUserLocation={true}
       userInterfaceStyle="light"
-    ></MapView>
+    >
+      {markers.map((marker) => (
+        <Marker
+          key={marker.id}
+          coordinate={{
+            latitude: marker.latitude,
+            longitude: marker.longitude,
+          }}
+          title={marker.title}
+          image={
+            selectedDriver === marker.id ? icons.selectedMarker : icons.marker
+          }
+        />
+      ))}
+    </MapView>
   );
 };
 
