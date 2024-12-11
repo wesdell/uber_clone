c01d3d8229156097f0c38606d4cc5714ab805aeb
Wesdell <wesdell17@gmail.com>
2024-12-11 17:57:18 -0500

    New find ride page which is fired when user chooses a destination

---
+++ app/(root)/find-ride.tsx
@@ -0,0 +1,46 @@
+import { Text, View } from "react-native";
+import { router } from "expo-router";
+import { useLocationStore } from "@/store";
+import { icons } from "@/constants";
+import RideLayout from "@/components/RideLayout";
+import GoogleTextInput from "@/components/GoogleTextInput";
+import CustomButton from "@/components/CustomButton";
+
+export default function FindRide() {
+  const {
+    userAddress,
+    destinationAddress,
+    setDestinationLocation,
+    setUserLocation,
+  } = useLocationStore();
+
+  return (
+    <RideLayout title="Ride" snapPoints={["85%"]}>
+      <View className="my-3">
+        <Text className="text-lg font-JakartaSemiBold mb-3">From</Text>
+        <GoogleTextInput
+          icon={icons.target}
+          initialLocation={userAddress!}
+          handlePress={(location) => setUserLocation(location)}
+          containerStyle="bg-neutral-100"
+          textInputBackgroundColor="#f5f5f5"
+        />
+      </View>
+      <View className="my-3">
+        <Text className="text-lg font-JakartaSemiBold mb-3">To</Text>
+        <GoogleTextInput
+          icon={icons.map}
+          initialLocation={destinationAddress!}
+          handlePress={(location) => setDestinationLocation(location)}
+          containerStyle="bg-neutral-100"
+          textInputBackgroundColor="#transparent"
+        />
+      </View>
+      <CustomButton
+        title="Find now"
+        onPress={() => router.push("/(root)/confirm-ride")}
+        className="mt-5"
+      />
+    </RideLayout>
+  );
+}
