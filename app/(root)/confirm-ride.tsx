856053463ffba7b883bbae35640784af01e79b0d
Wesdell <wesdell17@gmail.com>
2024-12-12 19:48:49 -0500

    Markup confirm ride page

---
+++ app/(root)/confirm-ride.tsx
@@ -0,0 +1,36 @@
+import { FlatList, View } from "react-native";
+import { router } from "expo-router";
+import DriveCard from "@/components/DriveCard";
+import RideLayout from "@/components/RideLayout";
+import CustomButton from "@/components/CustomButton";
+import { useDriverStore } from "@/store";
+
+const ConfirmRide = () => {
+  const { drivers, selectedDriver, setSelectedDriver } = useDriverStore();
+
+  return (
+    <RideLayout title="Choose a driver" snapPoints={["65%", "85%"]}>
+      <FlatList
+        data={drivers}
+        renderItem={({ item }) => (
+          <DriveCard
+            key={item.id}
+            rider={item}
+            selected={selectedDriver!}
+            setSelected={() => setSelectedDriver(Number(item.id!))}
+          />
+        )}
+        ListFooterComponent={() => (
+          <View className="mx-5 mt-10">
+            <CustomButton
+              title="Select ride"
+              onPress={() => router.push("/(root)/book-ride")}
+            />
+          </View>
+        )}
+      />
+    </RideLayout>
+  );
+};
+
+export default ConfirmRide;
