856553f1e54b6f925aef4b086d21851b06157378
Wesdell <wesdell17@gmail.com>
2024-12-13 19:39:28 -0500

    Fix geoapify uri to properly show static map

--- components/RideCard.tsx
+++ components/RideCard.tsx
@@ -10,9 +10,7 @@ const RideCard = ({ ride }: { ride: Ride }) => {
         <View className="flex flex-row items-center justify-between">
           <Image
             source={{
-              uri: `https://maps.geoapify.com/v1/staticmap?style=osm-bright&width=600&height=400&
-              center=lonlat:${ride.destination_longitude},${ride.destination_latitude}&zoom=14&
-              apiKey=${process.env.EXPO_PUBLIC_GEOAPIFY_API_KEY}`,
+              uri: `https://maps.geoapify.com/v1/staticmap?style=osm-bright&width=600&height=400&center=lonlat:${ride.destination_longitude},${ride.destination_latitude}&zoom=14&apiKey=${process.env.EXPO_PUBLIC_GEOAPIFY_API_KEY}`,
             }}
             className="w-[80px] h-[90px] rounded-lg"
           />
