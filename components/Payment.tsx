8defe34da1ec1ab1a2c456bb1431f31460aff213
Wesdell <wesdell17@gmail.com>
2024-12-14 19:21:31 -0500

    Implement stripe payments functionality

--- components/Payment.tsx
+++ components/Payment.tsx
@@ -1,8 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import { Alert } from "react-native";
+import { useStripe } from "@stripe/stripe-react-native";
 import CustomButton from "@/components/CustomButton";
+import { PaymentProps } from "@/types/type";
 
-const Payment = () => {
-  const openPaymentSheet = async () => {};
+const Payment = ({
+  fullName,
+  email,
+  amount,
+  driverId,
+  rideTime,
+}: PaymentProps) => {
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const [success, setSuccess] = useState(false);
+
+  const confirmHandler = async (paymentMethod, _, intentCreationCallback) => {
+    const { paymentIntent, customer } = await fetch("/(api)/(stripe)/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: fullName,
+        email,
+        amount,
+        paymentMethodId: paymentMethod.id,
+      }),
+    });
+
+    if (paymentIntent.client_secret) {
+      const { result } = await fetch("/(api)/(stripe)/pay", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          payment_method_id: paymentMethod.id,
+          payment_intent_id: paymentIntent.id,
+          customer_id: customer,
+        }),
+      });
+
+      if (result.client_secret) {
+      }
+    }
+    const { client_secret, error } = await response.json();
+    if (client_secret) {
+      intentCreationCallback({ clientSecret: client_secret });
+    } else {
+      intentCreationCallback({ error });
+    }
+  };
+
+  const initializePaymentSheet = async () => {
+    const { error } = await initPaymentSheet({
+      merchantDisplayName: "Example, Inc.",
+      intentConfiguration: {
+        mode: {
+          amount: 1099,
+          currencyCode: "USD",
+        },
+        confirmHandler: confirmHandler,
+      },
+    });
+    if (error) {
+    }
+  };
+
+  const openPaymentSheet = async () => {
+    await initializePaymentSheet();
+    const { error } = await presentPaymentSheet();
+
+    if (error) {
+      Alert.alert(`Error code: ${error.code}`);
+    } else {
+      setSuccess(true);
+    }
+  };
 
   return (
     <>
