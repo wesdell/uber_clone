f0782a4caaa0cd0e2dfa22c6852236d29ecff95a
Wesdell <wesdell17@gmail.com>
2024-12-15 13:23:05 -0500

    Change driver prop to item

--- components/DriveCard.tsx
+++ components/DriveCard.tsx
@@ -4,22 +4,22 @@ import { icons } from "@/constants";
 import { formatTime } from "@/utils/time";
 import { DriverCardProps } from "@/types/type";
 
-const DriverCard = ({ rider, selected, setSelected }: DriverCardProps) => {
+const DriverCard = ({ item, selected, setSelected }: DriverCardProps) => {
   return (
     <TouchableOpacity
       onPress={setSelected}
       className={`${
-        selected === rider.id ? "bg-general-600" : "bg-white"
+        selected === item.id ? "bg-general-600" : "bg-white"
       } flex flex-row items-center justify-between py-5 px-3 rounded-xl`}
     >
       <Image
-        source={{ uri: rider.profile_image_url }}
+        source={{ uri: item.profile_image_url }}
         className="w-14 h-14 rounded-full"
       />
 
       <View className="flex-1 flex flex-col items-start justify-center mx-3">
         <View className="flex flex-row items-center justify-start mb-1">
-          <Text className="text-lg font-JakartaRegular">{rider.title}</Text>
+          <Text className="text-lg font-JakartaRegular">{item.title}</Text>
 
           <View className="flex flex-row items-center space-x-1 ml-2">
             <Image source={icons.star} className="w-3.5 h-3.5" />
@@ -31,7 +31,7 @@ const DriverCard = ({ rider, selected, setSelected }: DriverCardProps) => {
           <View className="flex flex-row items-center">
             <Image source={icons.dollar} className="w-4 h-4" />
             <Text className="text-sm font-JakartaRegular ml-1">
-              ${rider.price}
+              ${item.price}
             </Text>
           </View>
 
@@ -40,7 +40,7 @@ const DriverCard = ({ rider, selected, setSelected }: DriverCardProps) => {
           </Text>
 
           <Text className="text-sm font-JakartaRegular text-general-800">
-            {formatTime(rider.time!)}
+            {formatTime(item?.time!)}
           </Text>
 
           <Text className="text-sm font-JakartaRegular text-general-800 mx-1">
@@ -48,13 +48,13 @@ const DriverCard = ({ rider, selected, setSelected }: DriverCardProps) => {
           </Text>
 
           <Text className="text-sm font-JakartaRegular text-general-800">
-            {rider.car_seats} seats
+            {item.car_seats} seats
           </Text>
         </View>
       </View>
 
       <Image
-        source={{ uri: rider.car_image_url }}
+        source={{ uri: item.car_image_url }}
         className="h-14 w-14"
         resizeMode="contain"
       />
