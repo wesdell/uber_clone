c42510f7389026b582e391c78822816c53766b06
Wesdell <wesdell17@gmail.com>
2024-12-15 13:24:27 -0500

    Define route when user select a place to go on

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,56 +1,95 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, Text, View } from "react-native";
 import MapView, { Marker, PROVIDER_DEFAULT } from "react-native-maps";
-import { useDriverStore, useLocationStore } from "@/store";
-import { calculateRegion, generateMarkersFromData } from "@/lib/map";
-import { driversMock } from "@/mocks/driver";
-import { MarkerData } from "@/types/type";
+import MapViewDirections from "react-native-maps-directions";
 import { icons } from "@/constants";
+import { useFetch } from "@/lib/fetch";
+import {
+  calculateDriverTimes,
+  calculateRegion,
+  generateMarkersFromData,
+} from "@/lib/map";
+import { useDriverStore, useLocationStore } from "@/store";
+import { Driver, MarkerData } from "@/types/type";
 
 const Map = ({ height }: { height: number }) => {
   const {
     userLongitude,
     userLatitude,
-    destinationLongitude,
     destinationLatitude,
+    destinationLongitude,
   } = useLocationStore();
   const { selectedDriver, setDrivers } = useDriverStore();
-  const region = calculateRegion({
-    userLongitude,
-    userLatitude,
-    destinationLongitude,
-    destinationLatitude,
-  });
+
+  const { data: drivers, loading, error } = useFetch<Driver[]>("/(api)/driver");
   const [markers, setMarkers] = useState<MarkerData[]>([]);
 
   useEffect(() => {
-    setDrivers(driversMock);
+    if (Array.isArray(drivers)) {
+      if (!userLatitude || !userLongitude) return;
 
-    if (Array.isArray(driversMock)) {
-      if (!userLatitude || !userLongitude) {
-        return;
-      }
+      const newMarkers = generateMarkersFromData({
+        data: drivers,
+        userLatitude,
+        userLongitude,
+      });
+
+      setMarkers(newMarkers);
     }
+  }, [drivers, userLatitude, userLongitude]);
 
-    const newMarkers = generateMarkersFromData({
-      data: driversMock,
-      userLatitude,
-      userLongitude,
-    });
-    setMarkers(newMarkers);
-  }, [userLatitude, userLongitude]);
+  useEffect(() => {
+    if (
+      markers.length > 0 &&
+      destinationLatitude !== undefined &&
+      destinationLongitude !== undefined
+    ) {
+      calculateDriverTimes({
+        markers,
+        userLatitude,
+        userLongitude,
+        destinationLatitude,
+        destinationLongitude,
+      }).then((drivers) => {
+        setDrivers(drivers as MarkerData[]);
+      });
+    }
+  }, [markers, destinationLatitude, destinationLongitude]);
+
+  const region = calculateRegion({
+    userLatitude,
+    userLongitude,
+    destinationLatitude,
+    destinationLongitude,
+  });
+
+  if (loading || (!userLatitude && !userLongitude))
+    return (
+      <View className="flex justify-between items-center w-full">
+        <ActivityIndicator size="small" color="#000" />
+      </View>
+    );
+
+  if (error)
+    return (
+      <View className="flex justify-between items-center w-full">
+        <Text>Error: {error}</Text>
+      </View>
+    );
 
   return (
     <MapView
       provider={PROVIDER_DEFAULT}
-      initialRegion={region}
-      style={{ height: height }}
+      className="w-full h-full rounded-2xl"
+      height={height}
       tintColor="black"
       mapType="mutedStandard"
       showsPointsOfInterest={false}
+      initialRegion={region}
       showsUserLocation={true}
       userInterfaceStyle="light"
     >
-      {markers.map((marker) => (
+      {markers.map((marker, index) => (
         <Marker
           key={marker.id}
           coordinate={{
@@ -59,10 +98,37 @@ const Map = ({ height }: { height: number }) => {
           }}
           title={marker.title}
           image={
-            selectedDriver === marker.id ? icons.selectedMarker : icons.marker
+            selectedDriver === +marker.id ? icons.selectedMarker : icons.marker
           }
         />
       ))}
+
+      {destinationLatitude && destinationLongitude && (
+        <>
+          <Marker
+            key="destination"
+            coordinate={{
+              latitude: destinationLatitude,
+              longitude: destinationLongitude,
+            }}
+            title="Destination"
+            image={icons.pin}
+          />
+          <MapViewDirections
+            origin={{
+              latitude: userLatitude!,
+              longitude: userLongitude!,
+            }}
+            destination={{
+              latitude: destinationLatitude,
+              longitude: destinationLongitude,
+            }}
+            apikey={process.env.EXPO_PUBLIC_GOOGLE_API_KEY!}
+            strokeColor="#0286FF"
+            strokeWidth={2}
+          />
+        </>
+      )}
     </MapView>
   );
 };
